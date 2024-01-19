81ee2a27711e14f87200c257cf7e20b1ac814628
User <fadhillatmojo@gmail.com>
2024-01-19 14:09:22 +0700

    change regis page to UI jadi

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,1 +1,218 @@
+import 'package:flutter/material.dart';
 
+class RegistrationScreen extends StatefulWidget {
+  const RegistrationScreen({super.key});
+
+  @override
+  State<RegistrationScreen> createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  // form key
+  final _formKey = GlobalKey<FormState>();
+
+  // editing controller
+  final TextEditingController firstNameController = TextEditingController();
+  final TextEditingController secondNameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    // image logo
+    final logoImage = Image.asset('assets/logo.png');
+
+    // firstName field
+    final firstNameField = TextFormField(
+      autofocus: false,
+      controller: firstNameController,
+      keyboardType: TextInputType.name,
+      onSaved: (value) {
+        firstNameController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+        prefixIcon: const Icon(Icons.person),
+        prefixIconColor: Colors.green,
+        contentPadding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+        hintText: 'Fadhill',
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: Colors.green),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        helperText: "Input Your firstName",
+      ),
+    );
+
+    // secondName field
+    final secondNameField = TextFormField(
+      autofocus: false,
+      controller: secondNameController,
+      keyboardType: TextInputType.name,
+      onSaved: (value) {
+        secondNameController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+        prefixIcon: const Icon(Icons.person),
+        prefixIconColor: Colors.green,
+        contentPadding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+        hintText: 'Atmojo',
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: Colors.green),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        helperText: "Input Your secondName",
+      ),
+    );
+    // email field
+    final emailField = TextFormField(
+      autofocus: false,
+      controller: emailController,
+      keyboardType: TextInputType.emailAddress,
+      onSaved: (value) {
+        emailController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+        prefixIcon: const Icon(Icons.email),
+        prefixIconColor: Colors.green,
+        contentPadding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+        hintText: 'youremail@gmail.com',
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: Colors.green),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        helperText: "Input Your Email",
+      ),
+    );
+
+    //pasword input
+    final passwordField = TextFormField(
+      autofocus: false,
+      controller: passwordController,
+      obscureText: true,
+      onSaved: (value) {
+        passwordController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+        prefixIcon: const Icon(Icons.key),
+        prefixIconColor: Colors.green,
+        contentPadding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+        hintText: '*****',
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: Colors.green),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        helperText: "Input Your password",
+      ),
+    );
+    //pasword input
+    final confirmPasswordField = TextFormField(
+      autofocus: false,
+      controller: confirmPasswordController,
+      obscureText: true,
+      onSaved: (value) {
+        confirmPasswordController.text = value!;
+      },
+      textInputAction: TextInputAction.done,
+      decoration: InputDecoration(
+        prefixIcon: const Icon(Icons.key),
+        prefixIconColor: Colors.green,
+        contentPadding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+        hintText: '*****',
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: Colors.green),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        helperText: "Input Your confirmPassword",
+      ),
+    );
+
+    // button register
+    final registerButton = Material(
+      elevation: 5,
+      borderRadius: BorderRadius.circular(30),
+      color: Colors.green,
+      child: MaterialButton(
+        onPressed: () {},
+        padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+        minWidth: MediaQuery.of(context).size.width,
+        child: const Text(
+          'Register',
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.white, fontSize: 20),
+        ),
+      ),
+    );
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text('Register Page'),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.all(36.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    logoImage,
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    firstNameField,
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    secondNameField,
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    emailField,
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    passwordField,
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    confirmPasswordField,
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    registerButton,
+                    const SizedBox(
+                      height: 20,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
